aef65f4b92a715f86a6904bdb0724962cd7aceb1
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2025-01-24 14:51:15 +0200

    WIP

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 (function ($) {
-    "use strict";
-    
+    'use strict';
+
     // Dropdown on mouse hover
     $(document).ready(function () {
         function toggleNavbarMethod() {
             if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
+                $('.navbar .dropdown')
+                    .on('mouseover', function () {
+                        $('.dropdown-toggle', this).trigger('click');
+                    })
+                    .on('mouseout', function () {
+                        $('.dropdown-toggle', this).trigger('click').blur();
+                    });
             } else {
                 $('.navbar .dropdown').off('mouseover').off('mouseout');
             }
@@ -17,8 +19,7 @@
         toggleNavbarMethod();
         $(window).resize(toggleNavbarMethod);
     });
-    
-    
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -28,22 +29,20 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
-
     // Date and time picker
     $('.date').datetimepicker({
-        format: 'L'
+        format: 'L',
     });
     $('.time').datetimepicker({
-        format: 'LT'
+        format: 'LT',
     });
 
-
     // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
+    $('.testimonial-carousel').owlCarousel({
         autoplay: true,
         smartSpeed: 1500,
         margin: 30,
@@ -51,20 +50,18 @@
         loop: true,
         center: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1,
             },
-            576:{
-                items:1
+            576: {
+                items: 1,
             },
-            768:{
-                items:2
+            768: {
+                items: 2,
             },
-            992:{
-                items:3
-            }
-        }
+            992: {
+                items: 2,
+            },
+        },
     });
-    
 })(jQuery);
-
